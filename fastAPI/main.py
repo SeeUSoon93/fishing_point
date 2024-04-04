e266633bace0e14292339de0a3b3629783b40f1f
김군순 <rlarnstns@gmail.com>
2024-04-05 00:41:13 +0900

    95%

--- fastAPI/main.py
+++ fastAPI/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Query, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, JSONResponse
+from typing import List
 from databases import Database
+from api.cors import create_app
+import logging
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("myapp")
+
+app = create_app()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
-database = Database("sqlite:///fishing_db.db")
+database = Database("sqlite:///fp.db")
 
 @app.on_event("startup")
 async def startup():
@@ -16,13 +23,81 @@ async def shutdown():
     await database.disconnect()
 
 @app.get("/main")
-def go_main():
+async def go_main():
     return FileResponse("index.html")
 
-@app.get("/location/{pointName}")
-async def get_location(pointName: str):
-    query = "SELECT xCoordi, yCoordi FROM fishingPoint WHERE pointName = :pointName"
-    result = await database.fetch_one(query=query, values={"pointName": pointName})
-    if result is None:
-        raise HTTPException(status_code=404, detail="Location not found")
-    return JSONResponse(content={"latitude": result["xCoordi"], "longitude": result["yCoordi"]})
+@app.get("/state_data")
+async def get_state():
+    # point_table에서 위치 목록 불러오기
+    query = "SELECT DISTINCT state FROM point_table"    
+    results = await database.fetch_all(query=query)
+    states = [result["state"] for result in results]
+
+    query2 = "SELECT DISTINCT UniqueFishSpecies FROM fish_table"    
+    results2 = await database.fetch_all(query=query2)
+    species = [result["UniqueFishSpecies"] for result in results2]
+
+    return JSONResponse(content={"states": states, "species": species})
+
+@app.get("/marker_data")
+async def get_marker():
+    query = "SELECT fpName, state, city, latitude, longitude, address FROM point_table"
+    results = await database.fetch_all(query=query)
+    fpNames = [result["fpName"] for result in results]
+    states = [result["state"] for result in results]
+    citys = [result["city"] for result in results]
+    latitudes = [result["latitude"] for result in results]
+    longitudes  = [result["longitude"] for result in results]
+    addresss = [result["address"] for result in results]
+
+    return JSONResponse(content={"fpNames": fpNames, "states":states, "citys":citys, "latitudes":latitudes, "longitudes":longitudes, "addresss":addresss})
+
+
+@app.get("/filter")
+async def filter_data(states: List[str] = Query(None), species: List[str] = Query(None)):
+    query = "SELECT * FROM point_table"
+
+    if species :
+        # 물고기 이름을 받아와서 번호 조회
+        species_placeholders = ", ".join([f"'{item}'" for item in species])
+        species_query = f"SELECT DISTINCT speciesIdx FROM fish_table WHERE UniqueFishSpecies IN ({species_placeholders})"
+        species_results = await database.fetch_all(query=species_query)
+        species_idxs = [result["speciesIdx"] for result in species_results]
+
+        if species_idxs:
+            species_idx_placeholders = ", ".join([str(idx) for idx in species_idxs])
+            point_idx_query = f"SELECT DISTINCT pointIdx FROM asso_table WHERE speciesIdx IN ({species_idx_placeholders})"
+            point_idx_results = await database.fetch_all(query=point_idx_query)
+            point_idxs = [result["pointIdx"] for result in point_idx_results]
+
+            if point_idxs:
+                point_idx_placeholders = ", ".join([str(idx) for idx in point_idxs])
+                query += f" WHERE pointIdx IN ({point_idx_placeholders})"
+
+    if states:
+        states_placeholders = ", ".join([f"'{state}'" for state in states])
+        if 'WHERE' in query:
+            query += f" AND state IN ({states_placeholders})"
+        else:
+            query += f" WHERE state IN ({states_placeholders})"   
+    
+    results = await database.fetch_all(query=query)
+
+    fpNames = [result["fpName"] for result in results]
+    stateList = [result["state"] for result in results]
+    citys = [result["city"] for result in results]
+    latitudes = [result["latitude"] for result in results]
+    longitudes  = [result["longitude"] for result in results]
+    addresss = [result["address"] for result in results]
+
+    return JSONResponse(content={"fpNames": fpNames, "states":stateList, "citys":citys, "latitudes":latitudes, "longitudes":longitudes, "addresss":addresss})
+
+@app.get("/fp_info/{fpName}")
+async def get_fp_info(fpName: str):
+    query = "SELECT * FROM point_table WHERE fpName = :fpName"
+    result = await database.fetch_one(query=query, values={"fpName": fpName})
+    if result:
+        return result
+    raise HTTPException(status_code=404, detail="Fishing point not found")
+
+
--- fastAPI/static/index.js
+++ fastAPI/static/index.js
@@ -1,93 +1,225 @@
-var map; // 지도 객체를 전역 변수로 선언
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(savePosition, showError);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-
-function savePosition(position) {
-    var userLocation = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-    };
-
-    // 사용자 위치를 기반으로 지도 생성
-    var mapContainer = document.getElementById('main_map'), // 지도를 표시할 div 
-        mapOption = {
-            center: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
-        };        
-
-    map = new kakao.maps.Map(mapContainer, mapOption); // 지도 객체 초기화
-
-    var marker = new kakao.maps.Marker({
-        map: map, 
-        position: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude)
-    });
-}
-
-function showError(error) {
-    console.warn(`ERROR(${error.code}): ${error.message}`);
-}
-
-getLocation();
-
-async function changeLocation(pointName) {
-    console.log(pointName);
-    const response = await fetch(`/location/${pointName}`);    
-    const data = await response.json();
-
-    if(map) { // map 객체가 초기화된 경우에만 실행
-        var moveLatLon = new kakao.maps.LatLng(data.latitude, data.longitude);
-        map.setCenter(moveLatLon);
-
-        var marker = new kakao.maps.Marker({
-            map: map, 
-            position: new kakao.maps.LatLng(data.latitude, data.longitude)
-        });
-
-        var content = '<div class="wrap">' + 
-            '    <div class="info">' + 
-            '        <div class="title">' + 
-            pointName +            
-            '            <div class="close" onclick="closeOverlay()" title="닫기"></div>' + 
-            '        </div>' + 
-            '        <div class="body">' + 
-            '            <div class="img">' +
-            '                <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png" width="73" height="70">' +
-            '           </div>' + 
-            '            <div class="desc">' + 
-            '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' + 
-            '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' + 
-            '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' + 
-            '            </div>' + 
-            '        </div>' + 
-            '    </div>' +    
-            '</div>';
-
-            var overlay = new kakao.maps.CustomOverlay({
-                content: content,
-                map: map,
-                position: marker.getPosition()       
-            });
-
-            // 마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-            kakao.maps.event.addListener(marker, 'click', function() {
-                overlay.setMap(map);
-            });
-
-            // 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
-            function closeOverlay() {
-                overlay.setMap(null);     
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            states: [],
+            species: [],
+            map: null,
+            markers: [],
+            fpNames: [],
+            latitudes: [],
+            longitudes: [],
+            addresss:[],
+            selectedStates:[],
+            selectedSpecie:[],
+            selectedFpInfo: null
+        };
+    },
+    async created() {
+        await this.fetchStates();
+        await this.fetchMarker();
+        await this.fetchFpInfo();
+    },
+    methods: {
+        async fetchStates() {
+            try {
+                const response = await fetch('/state_data');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                this.states = data.states;
+                this.species = data.species;
+            } catch (error) {
+                console.error('There was an error fetching the states:', error);
             }
+        },
+        // 좌표 정보 및 리스트 가져오기
+        async fetchMarker() {
+            try {
+                const response = await fetch('/marker_data');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                this.fpNames = data.fpNames;
+                this.latitudes = data.latitudes;
+                this.longitudes = data.longitudes;
+                this.addresss = data.addresss;
+                this.initMap();
+            } catch (error) {
+                console.error('There was an error fetching the species:', error);
+            }
+        },
 
+        // 필터링
+        async filterData(event) {
+            const value = event.target.value;
+            const isChecked = event.target.checked;
+            const isStateCheckbox = event.target.classList.contains('state-checkbox');
+            const isSpeciesCheckbox = event.target.classList.contains('species-checkbox');
+            
+            if (isStateCheckbox) {
+                if (isChecked) {
+                    this.selectedStates.push(value);
+                } else {
+                    const index = this.selectedStates.indexOf(value);
+                    if (index > -1) {
+                        this.selectedStates.splice(index, 1);
+                    }
+                }
+            }        
+            if (isSpeciesCheckbox) {
+                if (isChecked) {
+                    this.selectedSpecie.push(value);
+                } else {
+                    const index = this.selectedSpecie.indexOf(value);
+                    if (index > -1) {
+                        this.selectedSpecie.splice(index, 1);
+                    }
+                }
+            }        
+            const stateQueryParams = this.selectedStates.map(state => `states=${encodeURIComponent(state)}`).join('&');
+            const speciesQueryParams = this.selectedSpecie.map(specie => `species=${encodeURIComponent(specie)}`).join('&');
+            const queryParams = `${stateQueryParams}&${speciesQueryParams}`;
+        
+            const response = await fetch(`/filter?${queryParams}`, {
+                method: 'GET'
+            });
+            const filteredData = await response.json();
+            this.fpNames = filteredData.fpNames;
+            this.latitudes = filteredData.latitudes;
+            this.longitudes = filteredData.longitudes;
+            this.addresss = filteredData.addresss;
+            this.initMap();
+        },
 
+        initMap() {
+            const mapContainer = this.$refs.mapContainer;
+            const mapOption = {
+                center: new kakao.maps.LatLng(this.latitudes[0], this.longitudes[0]),
+                level: 3
+            };
+            this.map = new kakao.maps.Map(mapContainer, mapOption);
 
+            // 마커 이미지 설정
+            const imageSrc = 'static/img/marker.png',
+                imageSize = new kakao.maps.Size(64, 69),
+                imageOption = { offset: new kakao.maps.Point(27, 69) };
+            const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)            
+            
+            const positions = this.fpNames.map((name, index) => ({
+                content: `<div style="min-width: 250px; width: auto; max-width: 100%; font-family: Arial, sans-serif; border: 1px solid rgba(0,0,0,.1); border-radius: .25rem; background-color: #ffffff; box-shadow: 0 0.25rem 0.75rem rgba(0,0,0,.1);">
+                <div style="display: flex; justify-content: space-between; align-items: center; padding: .5rem .75rem; background-color: rgba(0,0,123,.06); border-bottom: 1px solid rgba(0,0,0,.125);">
+                  <strong style="font-size: .875rem; color: #007bff; white-space: nowrap;">${name}</strong>
+                  <button type="button" style="box-sizing: content-box; width: 1.5em; height: 1.5em; padding: 0; color: #6c757d; border: none; background: transparent; font-size: .875rem; cursor: pointer;" aria-label="Close">            
+                  </button>
+                </div>
+                <div style="padding: .75rem; font-size: .875rem; color: #6c757d; overflow-wrap: break-word;">
+                  <span>${this.addresss[index]}</span>
+                </div>
+            </div>
+            `,  // fpNames에서 이름 사용
+                latlng: new kakao.maps.LatLng(this.latitudes[index], this.longitudes[index])
+            }));
+            
+            let currentOverlay;
 
-    } else {
-        console.log("Map is not initialized");
+            positions.forEach((position) => {
+                const marker = new kakao.maps.Marker({
+                    map: this.map,  // Vue.js 데이터 속성에서 지도 객체 사용
+                    position: position.latlng,
+                    image: markerImage
+                });
+                const overlay = new kakao.maps.CustomOverlay({                                        
+                    content: position.content,
+                    position: marker.getPosition(),
+                    xAnchor: 0.5,
+                    yAnchor: 1.4
+                });
+
+                kakao.maps.event.addListener(marker, 'click', () =>{
+                    if (currentOverlay) {
+                        currentOverlay.setMap(null);
+                    }
+
+                    // 클릭된 마커에 해당하는 오버레이를 표시합니다.
+                    if (currentOverlay !== overlay) {
+                        overlay.setMap(this.map);
+                        currentOverlay = overlay; // 현재 활성화된 오버레이를 업데이트합니다.
+                    } else {
+                        currentOverlay = null; // 오버레이가 이미 활성화된 경우 참조를 제거합니다.
+                    }
+                });
+            });
+        },
+        
+        async fetchFpInfo(fpName) {
+            try {
+                console.log(fpName)                
+                const response = await fetch(`/fp_info/${fpName}`);                              
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                this.selectedFpInfo = data;
+                this.fpName = data.fpName;
+                this.latitude = data.latitude;
+                this.longitude = data.longitude;
+                this.address = data.address;
+                
+            const mapContainer = this.$refs.mapContainer;
+            const mapOption = {
+                center: new kakao.maps.LatLng(this.latitude, this.longitude),
+                level: 3
+            };
+            this.map = new kakao.maps.Map(mapContainer, mapOption);
+
+            // 마커 이미지 설정
+            const imageSrc = 'static/img/marker.png',
+                imageSize = new kakao.maps.Size(64, 69),
+                imageOption = { offset: new kakao.maps.Point(27, 69) };
+            const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)            
+            
+
+            const marker = new kakao.maps.Marker({
+                map: this.map,  // Vue.js 데이터 속성에서 지도 객체 사용
+                position: new kakao.maps.LatLng(this.latitude, this.longitude),
+                image: markerImage
+            });
+            const overlay = new kakao.maps.CustomOverlay({                                        
+                content: `<div style="min-width: 250px; width: auto; max-width: 100%; font-family: Arial, sans-serif; border: 1px solid rgba(0,0,0,.1); border-radius: .25rem; background-color: #ffffff; box-shadow: 0 0.25rem 0.75rem rgba(0,0,0,.1);">
+                <div style="display: flex; justify-content: space-between; align-items: center; padding: .5rem .75rem; background-color: rgba(0,0,123,.06); border-bottom: 1px solid rgba(0,0,0,.125);">
+                  <strong style="font-size: .875rem; color: #007bff; white-space: nowrap;">${this.fpName}</strong>
+                  <button type="button" style="box-sizing: content-box; width: 1.5em; height: 1.5em; padding: 0; color: #6c757d; border: none; background: transparent; font-size: .875rem; cursor: pointer;" aria-label="Close">            
+                  </button>
+                </div>
+                <div style="padding: .75rem; font-size: .875rem; color: #6c757d; overflow-wrap: break-word;">
+                  <span>${this.address}</span>
+                </div>
+            </div>
+            `,
+                position: marker.getPosition(),
+                xAnchor: 0.5,
+                yAnchor: 1.4
+            });            
+            let currentOverlay;
+            kakao.maps.event.addListener(marker, 'click', () =>{
+                if (currentOverlay) {
+                    currentOverlay.setMap(null);
+                }
+                if (currentOverlay !== overlay) {
+                    overlay.setMap(this.map);
+                    currentOverlay = overlay;
+                } else {
+                    currentOverlay = null;
+                }
+          });
+
+            } catch (error) {
+                console.error('There was an error fetching the states:', error);
+            }
+        }
+        
     }
-}
+});
